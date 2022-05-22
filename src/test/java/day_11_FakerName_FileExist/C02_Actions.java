77399abe5bf2b7a109e1ed33b49cef799ca2017e
yamangokhan <yamangokhanyaman@gmail.com>
2022-05-22 14:17:32 +0300

    22.05.2022

--- src/test/java/day_11_FakerName_FileExist/C02_Actions.java
+++ src/test/java/day_11_FakerName_FileExist/C02_Actions.java
@@ -1,4 +1,34 @@
 package day_11_FakerName_FileExist;
 
-public class C02_Actions {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebElement;
+import utilities.TestBase;
+
+public class C02_Actions extends TestBase {
+
+    @Test
+    public void test01(){
+
+        driver.get("https://html.com/tags/iframe/");
+
+
+
+            // calısıyor ama noırmalde calısmazsa actions ile obje olusuturup 3 adımda ısı halletmemziz lazım.
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        // once ıframi locate etmem lazım
+        WebElement Iframe = driver.findElement(By.xpath("//iframe[@src='https://www.youtube.com/embed/owsfdh4gxyc']"));
+        driver.switchTo().frame(Iframe);
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button']")).click();
+
+        WebElement youTubeYazisi = driver.findElement(By.xpath("//a[@class='ytp-youtube-button ytp-button yt-uix-sessionlink']"));
+
+        Assert.assertTrue(youTubeYazisi.isDisplayed());
+    }
 }
